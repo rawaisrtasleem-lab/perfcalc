03e718840901640e1526ae8060e16003131bbd76
Awais <rawaisrtasleem@gmail.com>
2026-05-16 22:32:19 -0700

    add microsoft clarity analytics tracking script

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,6 +91,21 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        
+        {/* Microsoft Clarity - Analytics tracking */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wse10ocw54");
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.variable} ${orbitron.variable} bg-[#0B0F19] text-gray-200 antialiased`}>
 
